93bfd546c5814bff15b886273fa25e7640f3abe2
manish gupta <chandelalmg2@gmail.com>
2026-07-29 18:49:34 +0530

    Time: 28 ms (69.58%) | Memory: 106.8 MB (16.34%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long sum=mass;
+        sort(asteroids.begin(),asteroids.end());
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]<=sum)sum+=asteroids[i];
+            else return false ;
+        }
+        return true;
+        
+    }
+};
