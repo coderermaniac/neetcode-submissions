f68e4410df28023bb5683cf0f1f41e5b287b1a9e
manish gupta <chandelalmg2@gmail.com>
2026-07-29 19:25:17 +0530

    Time: 10 ms (33.39%) | Memory: 133.4 MB (59.20%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<pivot)ans.push_back(nums[i]);
+        }
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]==pivot)ans.push_back(nums[j]);
+        }
+        for(int k=0;k<nums.size();k++){
+            if(nums[k]>pivot)ans.push_back(nums[k]);
+        }
+        return ans;
+
+
+    }
+};
