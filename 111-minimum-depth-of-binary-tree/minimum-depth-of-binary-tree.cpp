c40660b151df92d67833ada2f7fe054859c75a2b
manish gupta <chandelalmg2@gmail.com>
2026-08-08 17:33:52 +0530

    Time: 0 ms (100.00%) | Memory: 146.8 MB (79.04%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(!root->left && !root->right) return 1;
+
+        int Left = 0 , Right = 0;
+        if(root->left) Left = height(root->left);
+        if(root->right) Right = height(root->right);
+    
+        return (Left > 0 && Right > 0) ? min(Left,Right) + 1 : max(Left,Right) + 1;
+    }
+    int minDepth(TreeNode* root) {
+        if(!root) return 0;
+        return height(root);
+    }
+};
